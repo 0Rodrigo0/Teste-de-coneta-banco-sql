e1067c36107fb1024aa0bdd6f0b388794a8dc673
0Rodrigo0 <rodrigobl1@yahoo.com.br>
2022-02-27 11:45:36 -0300

    Projeto conecta banco de dados

--- src/ProdutoDao.java
+++ src/ProdutoDao.java
@@ -11,18 +11,19 @@ public class ProdutoDao {
     }
 
     public void cadastrar(Produto produto) {
-        try{
-        String sql = "INSERT INTO produto (nome, descricao, preco, qtd_estoque) VALUES (?,?,?,?)";
-        //sanitização dos inputs com prepareStatement
-        PreparedStatement ps = connection.prepareStatement(sql);
-        ps.setString(1, produto.getNome());
-        ps.setString(2, produto.getDescricao());
-        ps.setDouble(3, produto.getPreco());
-        ps.setString(4, produto.getQtd_estoque());
+        try {
+            String sql = "INSERT INTO produto (nome, descricao, preco, qtd_estoque) VALUES (?,?,?,?)";
+            //sanitização dos inputs com prepareStatement
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, produto.getNome());
+            ps.setString(2, produto.getDescricao());
+            ps.setDouble(3, produto.getPreco());
+            ps.setString(4, produto.getQtd_estoque());
 
-        ps.execute();
-        ps.close();
-    }catch (SQLException e){
-        System.out.println(e.getMessage());
+            ps.execute();
+            ps.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
